26e9abd7332bc391f65e5f23fddb7e5e128de616
Dheeraj Rauniyar <dheerajrauniyar123@gmail.com>
2023-09-18 21:20:31 +0530

    some file and contained added

---
+++ script.js
@@ -0,0 +1,29 @@
+const posswordBox = document.getElementById("passward");
+const length = 16;
+
+const cplatter = "ABCEDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const smlatter = "abcdefghijklmnopqrstuvwxyz";
+const numeric = "0123456789";
+const spsymbols = "!\"#$%&'()*+,-./:;<=>?[]";
+const allCt = cplatter + smlatter + numeric + spsymbols;
+
+
+const generatePassword = () => {
+    let password = " ";
+
+    password += cplatter[Math.floor(Math.random() * cplatter.length)];
+    password += smlatter[Math.floor(Math.random() * smlatter.length)];
+    password += numeric[Math.floor(Math.random() * numeric.length)];
+    password += spsymbols[Math.floor(Math.random() * spsymbols.length)];
+    while (length > password.length) {
+        password += allCt[Math.floor(Math.random() * allCt.length)];
+    }
+    posswordBox.value = password;
+
+}
+
+const copyPassword = () => {
+    posswordBox.select();
+    document.execCommand("copy");
+
+}
